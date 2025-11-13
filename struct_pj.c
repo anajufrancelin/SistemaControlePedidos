6799c976de88dfd3936aac7f8fceee3973a094d1
Ana Julia <franceliana007@gmail.com>
2025-11-12 23:46:39 -0300

    remove struct_pj.c da branch

--- struct_pj.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "struct_cliente.c" //Inclui a definicao de cliente
-typedef struct {
-    Cliente dados; // Inclui os dados basicos do cliente
-    char cnpj[20]; // CNPJ da empresa
-    char nomeContato[100]; // Nome do responsavel ou contato
-} PessoaJuridica;
